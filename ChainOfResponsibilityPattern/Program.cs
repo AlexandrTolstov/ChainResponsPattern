f4d42f852f327d778d55a1777409a238280abf41
ТолстовА <tolstov.alexandr.85@gmail.com>
2019-03-23 12:11:30 +0600

    1

--- ChainOfResponsibilityPattern/Program.cs
+++ ChainOfResponsibilityPattern/Program.cs
@@ -10,6 +10,86 @@ namespace ChainOfResponsibilityPattern
     {
         static void Main(string[] args)
         {
+            Receiver receiver = new Receiver(false, false, false, true);
+
+            PaymentHandler bankPaymentHandler = new BankPaymentHandler();
+            PaymentHandler moneyPaymentHnadler = new MoneyPaymentHandler();
+            PaymentHandler paypalPaymentHandler = new PayPalPaymentHandler();
+            PaymentHandler kaspiPaymentHandler = new KaspiPaymentHandler();
+            bankPaymentHandler.Successor = moneyPaymentHnadler;
+            moneyPaymentHnadler.Successor = paypalPaymentHandler;
+            paypalPaymentHandler.Successor = kaspiPaymentHandler;
+
+            bankPaymentHandler.Handle(receiver);
+
+            Console.Read();
+        }
+    }
+    class Receiver
+    {
+        // банковские переводы
+        public bool BankTransfer { get; set; }
+        // денежные переводы - WesternUnion, Unistream
+        public bool MoneyTransfer { get; set; }
+        // перевод через PayPal
+        public bool PayPalTransfer { get; set; }
+        //Каспий
+        public bool KaspiTransfer { get; set; }
+        public Receiver(bool bt, bool mt, bool ppt, bool kas)
+        {
+            BankTransfer = bt;
+            MoneyTransfer = mt;
+            PayPalTransfer = ppt;
+            KaspiTransfer = kas;
+        }
+    }
+    abstract class PaymentHandler
+    {
+        public PaymentHandler Successor { get; set; }
+        public abstract void Handle(Receiver receiver);
+    }
+
+    class BankPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver receiver)
+        {
+            if (receiver.BankTransfer == true)
+                Console.WriteLine("Выполняем банковский перевод");
+            else if (Successor != null)
+                Successor.Handle(receiver);
+        }
+    }
+
+    class PayPalPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver receiver)
+        {
+            if (receiver.PayPalTransfer == true)
+                Console.WriteLine("Выполняем перевод через PayPal");
+            else if (Successor != null)
+                Successor.Handle(receiver);
+        }
+    }
+    // переводы с помощью системы денежных переводов
+    class MoneyPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver receiver)
+        {
+            if (receiver.MoneyTransfer == true)
+                Console.WriteLine("Выполняем перевод через системы денежных переводов");
+            else if (Successor != null)
+                Successor.Handle(receiver);
+        }
+    }
+
+    class KaspiPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver receiver)
+        {
+            if (receiver.KaspiTransfer == true)
+                Console.WriteLine("Выполняем перевод через Kaspi");
+            else if (Successor != null)
+                Successor.Handle(receiver);
         }
     }
 }
